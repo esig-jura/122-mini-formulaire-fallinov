5600885bb76c22e5ca979e6bd850db6e8aa0df0d
Steve Fallet <steve.fallet@divtec.ch>
2024-04-17 15:42:47 +0200

    contrôle que les mdp sont identiques

--- js/main.js
+++ js/main.js
@@ -5,4 +5,27 @@
 
 'use strict';
 
-console.log('Test liaison JS');
+// Récupération du formulaire
+const formulaire = document.querySelector('form');
+const txtMDP = document.querySelector('#mdp');
+const txtMDPConfirm = document.getElementById('mdpc');
+console.log(formulaire, txtMDP, txtMDPConfirm);
+
+// Ecouter l'envoi du formulaire
+formulaire.addEventListener('submit', function coucou(event) {
+    // Stoppe l'envoi du formulaire
+    event.preventDefault();
+
+    // Récupére la valeur du mdp et de la confirmation du mdp
+    let mdp = txtMDP.value;
+    let mdpConfirm = txtMDPConfirm.value;
+
+    // Tester si les mdp sont identiques
+    if(mdp !== mdpConfirm) {
+        alert('Les mots de passes sont différents !!!')
+    } else {
+        alert('Compte créé avec succès !');
+        // Envoyer le formulaire
+        formulaire.submit();
+    }
+});
